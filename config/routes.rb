208c5c408fc3833085b182b863a413dc8208f82f
Kevin <lopezkevin175@gmail.com>
2018-11-02 07:37:50 -0400

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-   #remember to clean these up
-   devise_for :families, path: 'families', controllers: { sessions: "families/sessions" }
-   devise_for :teams, path: 'teams' , controllers: { sessions: "teams/sessions" }
+   devise_for :families, path: 'families', controllers: { sessions: "families/sessions" } #devise
+   devise_for :teams, path: 'teams' , controllers: { sessions: "teams/sessions" } #devise
    root to: 'pages#home'
-   get 'familyPage', to: 'pages#family'
-   get 'teamPage', to: 'pages#team'  
-   get 'family', to: 'family#index', as: :family_root 
-   get 'family/createPost', to: 'family#createPost'  
-   get 'family/teamSearch', to: 'family#teamSearch'   
-   get 'team', to: 'team#index', as: :team_root   
-   get 'team/posts', to: 'team#allPosts' 
-   get 'team/myTeam', to: 'team#team'  
+   get 'familyPage', to: 'pages#family' 
+   get 'teamPage', to: 'pages#team'
+   as :family do 
+      get 'family', to: 'family#index', as: :family_root 
+      get 'family/createPost', to: 'family#createPost'  
+      get 'family/teamSearch', to: 'family#teamSearch'  
+   end  
+   as :team do      
+      get 'team', to: 'team#index', as: :team_root   
+      get 'team/posts', to: 'team#allPosts' 
+      get 'team/myTeam', to: 'team#team'
+   end  
 end
+
