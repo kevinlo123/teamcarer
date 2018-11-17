2bd4f8468a89d0717bf63759cd1f2f8dd4ee55b7
Kevin <lopezkevin175@gmail.com>
2018-11-17 04:53:58 -0500

    removing migration for heroku

--- db/migrate/20181116222127_remove_unconfirmed_email.rb
+++ db/migrate/20181116222127_remove_unconfirmed_email.rb
@@ -1,5 +1,5 @@
 class RemoveUnconfirmedEmail < ActiveRecord::Migration[5.2]
    def change
-      remove_column :families, :unconfirmed_email, :string
+      # remove_column :families, :unconfirmed_email, :string
    end
 end
