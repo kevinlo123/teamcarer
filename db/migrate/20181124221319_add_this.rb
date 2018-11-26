7752a519e17c4fdc646075370d7008ad99d71245
Kevin <lopezkevin175@gmail.com>
2018-11-25 19:51:56 -0500

    fixing backend

--- db/migrate/20181124221319_add_this.rb
+++ db/migrate/20181124221319_add_this.rb
@@ -1,5 +1,10 @@
 class AddThis < ActiveRecord::Migration[5.2]
   def change
-      # remove_column :care_teams, :team_id, :integer
+   create_table :care_teams do |t|
+      t.string :team_name
+      t.string :team_statement
+ 
+      t.timestamps
+    end
   end
 end
