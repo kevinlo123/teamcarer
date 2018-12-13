5321af68db65c0ef73bce400cb468c2e515ddfa2
Kevin <lopezkevin175@gmail.com>
2018-12-12 23:01:36 -0500

    trying to fix emailer

--- app/controllers/families/registrations_controller.rb
+++ app/controllers/families/registrations_controller.rb
@@ -14,15 +14,16 @@ class Families::RegistrationsController < Devise::RegistrationsController
 
    #   POST /resource
    def create
-      if verify_recaptcha
-         super
-       else
-         build_resource(sign_up_params)
-         clean_up_passwords(resource)
-         flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-         flash.delete :recaptcha_error
-         render :new
-       end
+      super
+      # if verify_recaptcha
+      #    super
+      #  else
+      #    build_resource(sign_up_params)
+      #    clean_up_passwords(resource)
+      #    flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+      #    flash.delete :recaptcha_error
+      #    render :new
+      #  end
    end
 
    # GET /resource/edit
