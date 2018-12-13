bf2cd698833755b8a176771f5e1ea1006911378c
Kevin <lopezkevin175@gmail.com>
2018-12-12 23:08:54 -0500

    fixing recaptcha again

--- app/controllers/families/registrations_controller.rb
+++ app/controllers/families/registrations_controller.rb
@@ -14,16 +14,15 @@ class Families::RegistrationsController < Devise::RegistrationsController
 
    #   POST /resource
    def create
-      super
-      # if verify_recaptcha
-      #    super
-      #  else
-      #    build_resource(sign_up_params)
-      #    clean_up_passwords(resource)
-      #    flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      #    flash.delete :recaptcha_error
-      #    render :new
-      #  end
+      if verify_recaptcha
+         super
+       else
+         build_resource(sign_up_params)
+         clean_up_passwords(resource)
+         flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+         flash.delete :recaptcha_error
+         render :new
+       end
    end
 
    # GET /resource/edit
