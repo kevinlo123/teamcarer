a411416fcc6c89a85c2dac5cdc5e8eb6bae42176
Kevin <lopezkevin175@gmail.com>
2018-12-24 16:35:10 -0500

    fixing migrations

--- db/migrate/20181126192742_add_team_id_to_family.rb
+++
@@ -1,5 +0,0 @@
-class AddTeamIdToFamily < ActiveRecord::Migration[5.2]
-  def change
-   remove_column :teams, :care_team_id, :integer
-  end
-end
--- db/migrate/20181126194727_adding_team_i_dback.rb
+++
@@ -1,5 +0,0 @@
-class AddingTeamIDback < ActiveRecord::Migration[5.2]
-  def change
-   add_column :teams, :care_team_id, :integer   
-  end
-end
