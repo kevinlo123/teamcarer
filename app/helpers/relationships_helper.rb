4eeb419cfc79f5d959957b78e4035691e8558a7c
Kevin <lopezkevin175@gmail.com>
2019-01-06 01:16:19 -0500

    made select tag helpers for options and starting family sign up frontend

---
+++ app/helpers/relationships_helper.rb
@@ -0,0 +1,20 @@
+module RelationshipsHelper
+   def family_relationships
+      [
+         ['Mother', 'Mother'],
+         ['Father', 'Father'],
+         ['Son', 'Son'],
+         ['Daughter', 'Daughter'],
+         ['Brother', 'Brother'],
+         ['Sister', 'Sister'],
+         ['Grandson', 'Grandson'],    
+         ['Granddaughter', 'Granddaughter'],
+         ['Aunt', 'Aunt'],
+         ['Uncle', 'Uncle'], 
+         ['Cousin', 'Cousin'],         
+         ['Niece', 'Niece'], 
+         ['Nephew', 'Nephew'],
+         ['Other', 'Other']       
+      ]
+   end
+end
