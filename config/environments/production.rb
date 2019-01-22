425a7d3456f55d5500bc60a2b00e2b3b837fbe16
Kevin <lopezkevin175@gmail.com>
2019-01-22 00:58:06 -0500

    debugging mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
       :enable_starttls_auto => true
    }
 
+   config.action_mailer.default_options = { from: 'teamcarer.herokuapp.com' }
+   
+
 
    # Ignore bad email addresses and do not raise email delivery errors.
    # Set this to true and configure the email server for immediate delivery to raise delivery errors.
