423cad65731ce40140209e0d5344bf79ae46f717
Kevin <lopezkevin175@gmail.com>
2019-01-25 13:58:54 -0500

    frontend dashboards

--- app/controllers/families/registrations_controller.rb
+++ app/controllers/families/registrations_controller.rb
@@ -4,8 +4,7 @@ class Families::RegistrationsController < Devise::RegistrationsController
    include Accessible
    skip_before_action :check_user, except: [:new, :create]
    before_action :configure_sign_up_params, only: [:create]
-   
-   # before_action :configure_account_update_params, only: [:update]
+   before_action :configure_account_update_params, only: [:update]
 
    def new
       super 
@@ -38,9 +37,9 @@ class Families::RegistrationsController < Devise::RegistrationsController
    end
 
    # PUT /resource
-   # def update
-   #   super
-   # end
+   def update
+      super
+   end
 
    # DELETE /resource
    def destroy
@@ -63,9 +62,13 @@ class Families::RegistrationsController < Devise::RegistrationsController
       end
 
       # If you have extra params to permit, append them to the sanitizer.
-      # def configure_account_update_params
-      #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-      # end
+      def configure_account_update_params
+        devise_parameter_sanitizer.permit(:account_updatekeys, keys:[:firstname, :middlename, :lastname, :phone, :street, :city, :state, :zip, :emergency_name, :emergency_contact, :recipient_relation])
+      end
+
+      def after_update_path_for(resource)
+         "/families/edit"
+      end
 
       #   The path used after sign up.
       def after_sign_up_path_for(resource)
