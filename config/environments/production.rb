d126fdd9b92c6a089bf8d45177e1ff99bc86ce97
Kevin <lopezkevin175@gmail.com>
2019-04-17 16:31:02 -0400

    fixing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = false
    config.action_mailer.perform_caching = false
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+   config.action_mailer.default_url_options = { :host => 'teamcarer-sample.herokuapp.com' }
    config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
